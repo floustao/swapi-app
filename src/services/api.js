2d9b117c2a821e1fdf02e5a884facae84b9910b2
Florestan Lefevre <florestanlefevre@gmail.com>
2020-12-21 11:39:01 +0100

    Init

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+/**
+* APIs.
+*
+* Goal is to consolidate all of the API calls inside the service.
+*
+* TODO: error handling of your choice for server errors (ex: bugsnag).
+*/
+
+const BASE_SWAPI_URL = 'https://swapi.dev/api/';
