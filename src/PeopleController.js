7a1883d0cc276c140ce141446bcb01b71bdca9dc
Florestan Lefevre <florestanlefevre@gmail.com>
2020-12-21 12:57:41 +0100

    Add spinner

--- src/PeopleController.js
+++ src/PeopleController.js
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { camelizeKeys } from 'humps';
+import { SpinnerDotted } from 'spinners-react';
 
 import { getPeopleData } from './services/api';
 
 export default function PeopleController () {
   const [people, setPeople] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
   const [planetPages, setPlanetPages] = useState({});
 
   useEffect(() => {
     const fetchPeople = async () => {
+      setIsFetching(true);
       const apiResponse = await getPeopleData().then(data => data);
         const camelizedPeople = camelizeKeys(apiResponse.results)
         setPeople(camelizedPeople);
+        setIsFetching(false);
     };
     fetchPeople();
   }, []);
 
+  if (isFetching) {
+    return (
+      <SpinnerDotted />
+    );
+  }
+
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '30x auto' }}>
       {people.map((character, index) => {
         const {
           birthYear,
